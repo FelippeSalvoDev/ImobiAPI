1b0daaf3c371327237b1a4aaae8fe1b186fa113c
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2026-05-20 19:20:03 -0300

    chore: registra dependências dos use cases e repositórios no Program.cs

--- ImobiAPI.API/Program.cs
+++ ImobiAPI.API/Program.cs
@@ -1,10 +1,26 @@
+using ImobiAPI.Application.Interfaces;
+using ImobiAPI.Application.UseCases.CalcularCustos;
+using ImobiAPI.Application.UseCases.ConsultarMunicipio;
+using ImobiAPI.Domain.Services;
 using ImobiAPI.Infrastructure.Persistence;
+using ImobiAPI.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// repositórios
+builder.Services.AddScoped<IMunicipioRepository, MunicipioRepository>();
+builder.Services.AddScoped<ITabelaEmolumentosRepository, TabelaEmolumentosRepository>();
+
+// serviços de domínio
+builder.Services.AddScoped<CalculadorCustosService>();
+
+// use cases
+builder.Services.AddScoped<CalcularCustosUseCase>();
+builder.Services.AddScoped<ConsultarMunicipioUseCase>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
