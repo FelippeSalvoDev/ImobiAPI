defcd16975c25951a9d4cd0f12ccfb221451cafb
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2026-05-23 18:14:42 -0300

    feat: adiciona entidade UsoApiKey no domínio

---
+++ ImobiAPI.Domain/Entities/UsoApiKey.cs
@@ -0,0 +1,23 @@
+﻿namespace ImobiAPI.Domain.Entities;
+
+public class UsoApiKey
+{
+    public long Id { get; private set; }
+    public int ApiKeyId { get; private set; }
+    public string Endpoint { get; private set; }
+    public int StatusCode { get; private set; }
+    public DateTime CriadoEm { get; private set; }
+
+    protected UsoApiKey()
+    {
+        Endpoint = null!;
+    }
+
+    public UsoApiKey(int apiKeyId, string endpoint, int statusCode)
+    {
+        ApiKeyId = apiKeyId;
+        Endpoint = endpoint;
+        StatusCode = statusCode;
+        CriadoEm = DateTime.UtcNow;
+    }
+}
